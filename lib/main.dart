b4c20c0f1c61be651eb35a28ed8681fe6bf5ee4d
doggbmx <manuorticosta@gmail.com>
2022-08-30 23:31:21 -0400

    finished?

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,13 @@ class _MyApp2 extends State<MyAppPurete> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
     setState(() {
@@ -70,7 +77,7 @@ class _MyApp2 extends State<MyAppPurete> {
                   answerQuestion: _answerQuestion,
                   questionIndex: _questionIndex,
                 )
-              : Result(_totalScore)),
+              : Result(_totalScore, _resetQuiz)),
     );
   }
 }
